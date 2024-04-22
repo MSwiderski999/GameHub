8d7d048e9e09b4f90749a6f1e99d72f437f5b659
MSwiderski999 <mikolaj.sviderski@gmail.com>
2024-04-22 16:19:54 +0200

    checking for column of three

--- frontend/src/pages/CandyCrush/index.tsx
+++ frontend/src/pages/CandyCrush/index.tsx
@@ -4,7 +4,7 @@ import GameContainer from "../../components/GameContainer"
 
 const width = 9
 const colors = [
-    "blue",
+    "blue", 
     "green",
     "orange",
     "purple",
@@ -14,6 +14,17 @@ const colors = [
 
 export default function CandyCrush() {
     const [colorArrangement, setColorArrangement] = useState<string[]>([])
+
+    const checkForColumnOfThree = () => {
+        for (let i = 0; i < 69; i++) {
+            const columnOfThree: number[] = [i, i + width, i + width * 2]
+            const decidedColor: string = colorArrangement[i]
+
+            if (columnOfThree.every(cell => colorArrangement[cell] === decidedColor)) {
+                columnOfThree.forEach(cell => colorArrangement[cell] = "")
+            }
+        }
+    }
 
     const createBoard = () => {
         const randomColorArrangement = []
@@ -28,12 +39,24 @@ export default function CandyCrush() {
         createBoard()
     }, [])
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfThree()
+            setColorArrangement([...colorArrangement])
+        }, 100)
+        return () => clearInterval(timer)
+    }, [checkForColumnOfThree])
+
     return (
         <GameContainer>
             <div className="candy-crush">
                 <div className="board">
                     {colorArrangement.map((color, index) => (
-                        <img key={index} style={{backgroundColor: color}}/>
+                        <img
+                            key={index}
+                            style={{backgroundColor: color}}
+                            alt={color}
+                        />
                     ))}
                 </div>
             </div>
